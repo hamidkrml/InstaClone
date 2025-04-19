7fc706f317ebce4e55d616357ed9d0a68a0fdbea
hamidkrml <karimlihamid4@gmail.com>
2025-04-19 19:02:12 +0300

    Refactor ProfileView to include UserStatView for displaying user statistics and improve layout consistency

--- instaClone/Core/Profile/ProfileView.swift
+++ instaClone/Core/Profile/ProfileView.swift
@@ -8,59 +8,87 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    private let gridItems:[GridItem] = [
+        .init(.flexible(),spacing: 5),
+        .init(.flexible(),spacing: 5),
+        .init(.flexible(),spacing: 5)
+    ]
+    
     var body: some View {
         
         
-        VStack {
-            //Profil Karti
-            HStack(spacing: 10){
-                Image("ben")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80,height: 80)
-                    .clipShape(Circle())
-                
-                
-                UserStatView(value: 10, title: "post")
-                UserStatView(value: 49, title: "takipci")
-                UserStatView(value: 67, title: "takipciler")
+        NavigationStack {
+            ScrollView {
+                VStack{
+                    //Header
+                    VStack(spacing:10) {
+                        //Profil Karti
+                        HStack{
+                            Image("ben")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80,height: 80)
+                                .clipShape(Circle())
+                            
+                            Spacer()
+                            HStack(spacing: 8){
+                                UserStatView(value: 10, title: "post")
+                                UserStatView(value: 49, title: "takipci")
+                                UserStatView(value: 67, title: "takipcile")
+                            }
+                            
+                        }
+                        .padding(.horizontal)
+                        
+                        // Ad ve aciklama kismi
+                        VStack(alignment: .leading,spacing: 4){
+                            Text("Hamid Karimli")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            Text("Ios Devloper ")
+                                .font(.footnote)
+                            
+                        }
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                        .padding(.horizontal)
+                        
+                        //Edit Button
+                        Button{
+                            
+                        }label: {
+                            Text("EditProfile")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity,minHeight: 32)
+                                .foregroundStyle(Color("ButtonText"))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 6)
+                                        .stroke(.gray,lineWidth: 2)
+                                )}
+                        Divider()
+                    }
+                    LazyVGrid(columns: gridItems,spacing: 1){
+                        ForEach(0...9,id: \.self){ index in
+                            Image(systemName: "swift")
+                                .resizable()
+                                .scaledToFill()
+                                .padding(.bottom)
+                        }
+                    }
+                }
             }
-            
-            // Ad ve aciklama kismi
-            VStack(alignment: .leading,spacing: 10){
-                Text("Hamid Karimli")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
-                Text("Ios Devloper ")
-                    .font(.footnote)
-                
+            .navigationTitle("profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing){
+                    Button{}label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(Color("ButtonText"))
+                    }
+                }
             }
-            .frame(maxWidth: .infinity,alignment: .leading)
-            .padding(.horizontal)
-            
-            
-            
-            
-            //Edit Button
-            Button{
-                
-            }label: {
-                Text("EditProfile")
-                    .font(.subheadline)
-                    .fontWeight(.bold)
-                    .frame(width: 360,height: 32)
-                    .foregroundStyle(Color("ButtonText"))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(.gray,lineWidth: 2)
-                    )
-            }
-            Divider()
-            
         }
-        
-        
-        
     }
 }
 
