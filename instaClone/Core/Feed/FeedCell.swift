042a876a6a2df824e43797f0bb224ee5ff6484cb
hamidkrml <karimlihamid4@gmail.com>
2025-04-19 19:54:44 +0300

    Add FeedCell and FeedView

---
+++ instaClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,35 @@
+//
+//  FeedCell.swift
+//  instaClone
+//
+//  Created by hamid on 19.04.2025.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack{
+            // profil Fotografi ve Kullanci adi
+        
+            
+            
+            // post Goreseli
+            
+            
+            
+            //action button
+            
+            
+            //begeni sayi ile like
+            
+            
+            // aciklama kismi 
+        }
+
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ instaClone/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  instaClone
+//
+//  Created by hamid on 19.04.2025.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
